3ceb1e400cb0815b4a23bb177b3758e403126992
Trần Phước Định <dinhtppc00576@fpt.edu.vn>
2021-10-20 08:16:45 +0700

    Clear warning Lottie

--- src/App.js
+++ src/App.js
@@ -3,17 +3,7 @@ import Header from "./layouts/Header";
 import NavMobile from "./layouts/NavMobile";
 import Sidebar from "./layouts/Sidebar";
 import React from "react";
-import "jquery/dist/jquery.min.js";
-import "datatables.net-dt/css/jquery.dataTables.min.css";
-import "datatables.net-dt/js/dataTables.dataTables";
-import "datatables.net-buttons/js/dataTables.buttons.js";
-import "datatables.net-buttons/js/buttons.colVis.js";
-import "datatables.net-buttons/js/buttons.flash.js";
-import "datatables.net-buttons/js/buttons.html5.js";
-import "datatables.net-buttons/js/buttons.print.js";
 import { useEffect } from "react";
-const jzip = require("jzip");
-window.JSZip = jzip;
 
 function App() {
   useEffect(() => {
@@ -51,6 +41,8 @@ function App() {
       window.onresize = function () {
         setHeight();
       };
+
+      console.clear();
       setHeight();
     }, 700);
   }, []);
