dafb755d27fa8ac8a6e47d1fa6e1d0f6a1db9d64
Trần Phước Định <dinhtppc00576@fpt.edu.vn>
2021-10-21 12:16:14 +0700

    Fix lỗi không search được data khi render

--- src/utils/datatable/configData.js
+++ src/utils/datatable/configData.js
@@ -3,13 +3,21 @@ export const headingsLoai = ["Mã loại", "Tên loại"];
 export const columnsLoai = [
   {
     data: "id",
-    render: (data, type, row, meta) =>
-      `<input id="id" style="border: none;background-color: rgba(0, 0, 0, 0);" value="${data}" />`,
+    render: (data, type, row, meta) => {
+      if (type === "display") {
+        data = `<input id="id" style="border: none;background-color: rgba(0, 0, 0, 0);" value="${data}" />`;
+      }
+      return data;
+    },
   },
   {
-    data: "last_name",
-    render: (data, type, row, meta) =>
-      `<input id="name" style="border: none;background-color: rgba(0, 0, 0, 0);" value="${data}" />`,
+    data: "name",
+    render: (data, type, row, meta) => {
+      if (type === "display") {
+        data = `<input id="name" style="border: none;background-color: rgba(0, 0, 0, 0);" value="${data}" />`;
+      }
+      return data;
+    },
   },
 ];
 export const urlLoai = "https://reqres.in/api/users?page=2";
