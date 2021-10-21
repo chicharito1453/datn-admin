df69593c995eea89cc476d9fcc099665d0c05e7e
Trần Phước Định <dinhtppc00576@fpt.edu.vn>
2021-10-21 16:05:34 +0700

    Chỉnh sửa useEffect

--- src/utils/datatable/Datatable.js
+++ src/utils/datatable/Datatable.js
@@ -8,40 +8,43 @@ import "datatables.net-buttons/js/buttons.colVis.js";
 import "datatables.net-buttons/js/buttons.flash.js";
 import "datatables.net-buttons/js/buttons.html5.js";
 import "datatables.net-buttons/js/buttons.print.js";
-import { useEffect } from "react";
+import { useEffect, useReducer } from "react";
 const jzip = require("jzip");
 window.JSZip = jzip;
 
-const Datatable = ({ id, headings, columns, data, addEvents, test }) => {
+const Datatable = ({ id, headings, columns, data, addEvents, myReducer }) => {
+  const [list, dispatch] = useReducer(myReducer, data);
+
   useEffect(() => {
     var table = $("#" + id).DataTable({
       ...configTable,
       columns,
     });
+    if (data.length === 0) return;
     table.clear().draw();
     table.rows.add(data).draw();
-    // addEvents.forEach((addEvent) => {
-    //   const myFunction = setInterval(() => {
-    //     var chooseer = document.querySelectorAll(
-    //       "#" + id + " " + addEvent.selector
-    //     );
-    //     if (chooseer.length > 0) {
-    //       chooseer.forEach((element) => {
-    //         element.addEventListener(addEvent.event, () =>
-    //           addEvent.operation(element.value, 2)
-    //         );
-    //       });
-    //       clearInterval(myFunction);
-    //     }
-    //   }, 10);
-    // });
+    // Them su kien
+    addEvents.forEach((addEvent) => {
+      const myFunction = setInterval(() => {
+        var chooseer = document.querySelectorAll(
+          "#" + id + " " + addEvent.selector
+        );
+        if (chooseer.length > 0) {
+          chooseer.forEach((element) => {
+            element.addEventListener(addEvent.event, () =>
+              dispatch({ type: addEvent.operation, thaotac: addEvent.thaotac })
+            );
+          });
+          clearInterval(myFunction);
+        }
+      }, 10);
+    });
   }, [id, columns, data, addEvents]);
 
   return (
     <div style={{ height: 600 }} className="table-responsive">
       <br />
       <table
-        onChange={test}
         id={id}
         className="table table-striped table-borderless table-hover table-md table-responsive-sm"
         cellSpacing="0"
