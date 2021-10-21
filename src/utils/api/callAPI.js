8ed1399c3f32d8511fe3f8b38a328366948bf33d
Trần Phước Định <dinhtppc00576@fpt.edu.vn>
2021-10-21 12:14:05 +0700

    Chuyển thành async await function

--- src/utils/api/callAPI.js
+++ src/utils/api/callAPI.js
@@ -1,12 +1,15 @@
 import axios from "axios";
 
-function callAPI(url, method = "GET", data = null) {
-  return axios({
-    method,
-    url,
-    data,
-  }).catch((error) => {
-    console.log("Error: ", error);
-  });
+async function callAPI(url, method = "GET", data = null) {
+  try {
+    const resp = await axios({
+      url,
+      method,
+      data,
+    });
+    return [null, resp];
+  } catch (error) {
+    return [error, null];
+  }
 }
 export default callAPI;
