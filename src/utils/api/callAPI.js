702b6d7eeea0c33440054388ada84c0503f4107e
Trần Phước Định <dinhtppc00576@fpt.edu.vn>
2021-10-30 14:08:09 +0700

    Lấy url từ dotenv

--- src/utils/api/callAPI.js
+++ src/utils/api/callAPI.js
@@ -3,7 +3,7 @@ import axios from "axios";
 async function callAPI(endpoint, method = "GET", data = null) {
   try {
     const resp = await axios({
-      url: "http://localhost:8080/api/v1" + endpoint,
+      url: process.env.REACT_APP_API + endpoint,
       method,
       data,
     });
