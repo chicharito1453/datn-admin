49891624369687373a3d799070a3ea5c8c1c6ed7
Trần Phước Định <dinhtppc00576@fpt.edu.vn>
2021-10-31 22:38:10 +0700

    Tạo hàm upload ảnh trong file common.js

--- public/assets/okteam/common.js
+++ public/assets/okteam/common.js
@@ -21,6 +21,19 @@ function isOK(message) {
   return message == "OK";
 }
 
+// upload ảnh
+async function upload(image) {
+  try {
+    const dataImage = new FormData();
+    dataImage.append("upload_preset", REACT_APP_UPLOAD_PRESET);
+    dataImage.append("file", image);
+    const resp = await axios.post(REACT_APP_UPLOAD_URL, dataImage);
+    return [null, resp];
+  } catch (error) {
+    return [error, null];
+  }
+}
+
 //gọi api
 async function callAPI(endpoint, method = "GET", data = null) {
   try {
