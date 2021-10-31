9de08e5a818c0ab2bcf712eb0b24740d09f25c68
Trần Phước Định <dinhtppc00576@fpt.edu.vn>
2021-10-31 13:32:54 +0700

    Test hàm và tham số mới

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,8 +1,23 @@
-function update_loai(idLoai, value, thaotac) {
-  console.log(idLoai, value, thaotac);
+async function update_loai(idLoai, value, thaotac, oldValue, element) {
+  console.log();
+  if (value.trim() == "") {
+    element.value = oldValue;
+    Fail("Không để trống!");
+    return;
+  }
+  const [error, resp] = await callAPI("/category/list");
+  if (error) {
+    Fail("Không thực hiện được thao tác!");
+    console.log(error);
+    return false;
+  }
+  const { result, message } = resp.data;
+  console.log(result);
 }
 function setImgLoai() {
   document.querySelector("#anhLoai").click();
 }
 
-function delete_loai(id) {}
+function delete_loai(id, index) {
+  $("#dataTable").deleteRow(index);
+}
