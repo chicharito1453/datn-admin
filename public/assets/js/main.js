ae2c261b1f59fa76a5b6af7b9b2fe9d697308f4c
Trần Phước Định <dinhtppc00576@fpt.edu.vn>
2021-10-31 15:57:07 +0700

    Hoàn thành chức năng cập nhật tên loại và parent

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,18 +1,27 @@
-async function update_loai(idLoai, value, thaotac, oldValue, element) {
-  console.log();
-  if (value.trim() == "") {
-    element.value = oldValue;
-    Fail("Không để trống!");
-    return;
-  }
-  const [error, resp] = await callAPI("/category/list");
+async function update_loai(idcate, value, thaotac, oldValue, element) {
+  const [error, resp] = await callAPI(
+    `/category/update?idcate=${idcate}&value=${value.trim()}&thaotac=${thaotac}`,
+    "PUT"
+  );
   if (error) {
     Fail("Không thực hiện được thao tác!");
     console.log(error);
     return false;
   }
-  const { result, message } = resp.data;
-  console.log(result);
+  const { message } = resp.data;
+  if (!isOK(message)) {
+    Fail(message);
+    if (thaotac == "0") element.value = oldValue;
+    if (thaotac == "2") {
+      element.value = oldValue == "null" ? "" : oldValue;
+    }
+    return false;
+  }
+  if (thaotac == "0") {
+    element.value = value;
+  }
+  Success("Cập nhật thành công!");
+  return true;
 }
 function setImgLoai() {
   document.querySelector("#anhLoai").click();
