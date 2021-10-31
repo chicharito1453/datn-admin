5439448ad82556f133ee9e7de2278bb55dd29086
Trần Phước Định <dinhtppc00576@fpt.edu.vn>
2021-10-31 13:33:33 +0700

    Thêm hàm gọi api và các biến selector

--- public/assets/okteam/common.js
+++ public/assets/okteam/common.js
@@ -1,7 +1,32 @@
+// url api okteam
+const REACT_APP_API = "http://localhost:8080/api/v1";
+const REACT_APP_UPLOAD_PRESET = "x2qgwhlm";
+const REACT_APP_UPLOAD_URL =
+  "https://api.cloudinary.com/v1_1/okteam/image/upload";
+
+// selector
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+//thông báo
 const Success = (message, title = "Thông báo") => {
   swal({ icon: "success", title: title, text: message });
 };
 
-const Fail = (message, title = "Thông báo") => {
+const Fail = (message, title = "Có lỗi xảy ra!") => {
   swal({ icon: "error", title: title, text: message });
 };
+
+//gọi api
+async function callAPI(endpoint, method = "GET", data = null) {
+  try {
+    const resp = await axios({
+      method,
+      url: REACT_APP_API + endpoint,
+      data,
+    });
+    return [null, resp];
+  } catch (error) {
+    return [error, null];
+  }
+}
