0bb67d950a69e9aa349357aa055a4d5e1a757d62
Trần Phước Định <dinhtppc00576@fpt.edu.vn>
2021-11-03 22:04:38 +0700

    Thêm heading nhãn hàng

--- src/utils/datatable/headings.js
+++ src/utils/datatable/headings.js
@@ -1,2 +1,4 @@
-// table loại
-export const headingLoai = ["Mã loại", "Tên loại", "Ảnh", "Loại cha", ""];
+// loại
+export const headingLoai = ["Mã loại", "Tên loại", "Ảnh", "Loại cha", "#"];
+// nhãn
+export const headingNhan = ["STT", "Tên nhãn ", "#"];
