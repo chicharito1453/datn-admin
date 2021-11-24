a9f7828d52e8d045a660d0c9a848950f4d8ae274
Trần Phước Định <dinhtppc00576@fpt.edu.vn>
2021-11-24 22:44:32 +0700

    Thêm headers cho axios

--- src/utils/api/okteamAPI.js
+++ src/utils/api/okteamAPI.js
@@ -1,11 +1,14 @@
 import axios from "axios";
+import { getToken } from "../localStorage/localStorage";
 
 async function okteamAPI(endpoint, method = "GET", data = null) {
+  const token = getToken();
   try {
     const resp = await axios({
       url: process.env.REACT_APP_API + endpoint,
       method,
       data,
+      headers: { Authorization: "Bearer " + token },
     });
     return [null, resp];
   } catch (error) {
