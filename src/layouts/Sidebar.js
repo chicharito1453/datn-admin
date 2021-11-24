75672f6abcd1c745a384a23adccc6fd7a9012eeb
Trần Phước Định <dinhtppc00576@fpt.edu.vn>
2021-11-24 20:17:03 +0700

    Hiển thị thông tin người dùng khi đăng nhập

--- src/layouts/Sidebar.js
+++ src/layouts/Sidebar.js
@@ -1,15 +1,27 @@
 import Menu from "../components/Menu";
-import { memo } from "react";
+import { getFromLocalStorage } from "../utils/localStorage/localStorage";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="profile_info">
-        <img src="/assets/img/avatar.png" className="profile_image" alt="" />
-        <h4>Định</h4>
+        <img
+          src={
+            getFromLocalStorage("myData")
+              ? getFromLocalStorage("myData").image
+              : "/assets/img/avatar.png"
+          }
+          className="profile_image"
+          alt=""
+        />
+        <h4>
+          {getFromLocalStorage("myData")
+            ? getFromLocalStorage("myData").username
+            : "Vui lòng đăng nhập!"}
+        </h4>
       </div>
       <Menu />
     </div>
   );
 };
-export default memo(Sidebar);
+export default Sidebar;
