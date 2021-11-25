91f70968b13094340584a9bde34e36c77d096b20
Trần Phước Định <dinhtppc00576@fpt.edu.vn>
2021-11-25 16:37:59 +0700

    Sắp xếp thứ tự import

--- src/views/categories/table/TableLoai.js
+++ src/views/categories/table/TableLoai.js
@@ -1,6 +1,6 @@
+import { memo } from "react";
 import Datatable from "../../../utils/datatable/Datatable";
 import { headingLoai } from "../../../utils/datatable/headings";
-import { memo } from "react";
 import configLoai from "../../../utils/datatable/config/configLoai";
 
 const TableLoai = ({ data, deleted }) => {
