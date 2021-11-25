604164b8ce5b0b7d1e8cb9833a92a069fa546326
Trần Phước Định <dinhtppc00576@fpt.edu.vn>
2021-11-25 09:17:36 +0700

    Chuyển chức năng đóng mở nav qua từ App.js

--- src/layouts/NavMobile.js
+++ src/layouts/NavMobile.js
@@ -1,6 +1,21 @@
+import { useEffect } from "react";
 import Menu from "../components/Menu";
 
 const NavMobile = () => {
+  useEffect(() => {
+    const $ = document.querySelector.bind(document);
+    function toggleNav() {
+      $(".mobile_nav_items").classList.toggle("active");
+    }
+
+    // Đóng mở nav khi responsive
+    $(".nav_btn").addEventListener("click", toggleNav);
+
+    return () => {
+      $(".nav_btn").removeEventListener("click", toggleNav);
+    };
+  }, []);
+
   return (
     <div className="mobile_nav">
       <div className="nav_bar">
