57c3ac216715ec93912659bc336ba2915d4b7477
Trần Phước Định <dinhtppc00576@fpt.edu.vn>
2021-11-25 16:40:08 +0700

    Tạo file viết chức năng update Ncc

---
+++ public/assets/js/ncc.js
@@ -0,0 +1,9 @@
+async function update_ncc(username, value, thaotac, oldValue, element) {
+  console.log(username);
+}
+async function update_trangthai_ncc(username, isChecked, element) {
+  console.log(username);
+}
+function setImgNcc(username) {
+  document.querySelector(`#file_ncc_${username}`).click();
+}
