badfdfe06810c9f7d8e4ab1c12aeeee0ddd4bc5b
Trần Phước Định <dinhtppc00576@fpt.edu.vn>
2021-11-26 19:25:42 +0700

    Viết chức năng lấy danh sách Ctv

--- src/views/ctv/CTV.js
+++ src/views/ctv/CTV.js
@@ -1,19 +1,40 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { Modal, Button } from "react-bootstrap";
+import { connect } from "react-redux";
+import { ALL_CTV } from "../../store/action/index";
+import okteamAPI from "../../utils/api/okteamAPI";
+import { Fail, isOK } from "../../utils/sweetalert2/alert";
 import TableCtv from "./table/TableCtv";
 import FormCtv from "./form/FormCtv";
 
-const CTV = () => {
+const CTV = ({ data, getAllCtv }) => {
   const [show, setShow] = useState(false);
-
-  useEffect(() => {
-    if (!document.title) document.title = "Quản trị - Cộng tác viên";
-    document.querySelector(".content").style.height = "auto";
-  }, []);
 
   function handleClose() {
     setShow(false);
   }
+
+  // DANH SÁCH CỘNG TÁC VIÊN
+  const list_Ctv = useCallback(async () => {
+    const [error, resp] = await okteamAPI("/ctv/list");
+    if (error) {
+      Fail("Không thực hiện được thao tác!");
+      console.log(error);
+      return false;
+    }
+    const { result, message } = resp.data;
+    if (!isOK(message)) {
+      Fail(message);
+      return false;
+    }
+    getAllCtv(result);
+    document.querySelector(".content").style.height = "auto";
+  }, [getAllCtv]);
+
+  useEffect(() => {
+    if (!document.title) document.title = "Quản trị - Cộng tác viên";
+    list_Ctv();
+  }, [list_Ctv]);
 
   return (
     <div className="container">
@@ -27,7 +48,7 @@ const CTV = () => {
       </Button>
       <br />
       <br />
-      <TableCtv />
+      <TableCtv data={data} />
       <Modal
         size="lg"
         show={show}
@@ -43,4 +64,19 @@ const CTV = () => {
     </div>
   );
 };
-export default CTV;
+
+const mapStatetoProps = (state) => {
+  return {
+    data: state.list_Ctv,
+  };
+};
+
+const mapDispatchToProps = (dispatch, props) => {
+  return {
+    getAllCtv: (list) => {
+      dispatch(ALL_CTV(list));
+    },
+  };
+};
+
+export default connect(mapStatetoProps, mapDispatchToProps)(CTV);
