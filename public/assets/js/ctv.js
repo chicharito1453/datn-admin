00f4876e9c96957e6eb6df4ba01c97c6db49cd76
Trần Phước Định <dinhtppc00576@fpt.edu.vn>
2021-11-26 19:20:42 +0700

    Thêm file update Ctv

---
+++ public/assets/js/ctv.js
@@ -0,0 +1,7 @@
+async function update_ctv(username, value, thaotac, oldValue, element) {
+  console.log(value);
+}
+async function update_trangthai_ctv(username, isChecked, element) {}
+function setImgNcc(username) {
+  document.querySelector(`#file_ctv_${username}`).click();
+}
