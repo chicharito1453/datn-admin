221a8c839f80896a37a5d6bbf7adede9bc051402
Trần Phước Định <dinhtppc00576@fpt.edu.vn>
2021-11-26 19:23:41 +0700

    Thêm config datatable cho Ctv

---
+++ src/utils/datatable/config/configCtv.js
@@ -0,0 +1,167 @@
+const configCtv = {
+  info: false,
+  ordering: false,
+  destroy: true,
+  pagingType: "full_numbers",
+  pageLength: 5,
+  lengthChange: false,
+  language: {
+    emptyTable: "<b style='color:red'>Không có dữ liệu</b>",
+  },
+  dom: "Blfrtip",
+  buttons: [
+    {
+      extend: "copy",
+      text: '<div><i class="fas fa-copy"></i> Copy</div>',
+      className: "btn btn-primary",
+      exportOptions: { orthogonal: "export" },
+    },
+    {
+      extend: "excelHtml5",
+      text: '<div><i class="far fa-file-excel"></i> Excel</div>',
+      className: "btn btn-success",
+      exportOptions: { orthogonal: "export" },
+    },
+    {
+      extend: "csv",
+      text: '<div><i class="fas fa-file-csv"></i> CSV</div>',
+      className: "btn btn-secondary",
+      exportOptions: { orthogonal: "export" },
+    },
+    {
+      extend: "print",
+      text: '<div><i class="fas fa-print" style="width: 20px;"></i> Print</div>',
+      className: "btn btn-danger ",
+      exportOptions: { orthogonal: "export" },
+    },
+  ],
+  columns: [
+    {
+      data: "username",
+      render: (data, type, row, meta) => {
+        return `<i style="cursor:pointer" class="far fa-trash-alt"></i>`;
+      },
+    },
+    {
+      data: "active",
+      render: (data, type, row, meta) => {
+        if (type === "display") {
+          return `<input type="checkbox" onchange="update_trangthai_ctv('${
+            row.username
+          }', this.checked, this)" style="width: 100px;" ${
+            data && "checked"
+          } />`;
+        }
+        if (type === "export") {
+          return data ? "Đã kích hoạt" : "Vô hiệu hóa";
+        }
+        return data;
+      },
+    },
+    {
+      data: "username",
+    },
+    {
+      data: "password",
+      render: (data, type, row, meta) => {
+        return `<input type="password" onchange="update_ctv('${row.username}', this.value, 0, '${data}', this)"  value="${data}" />`;
+      },
+    },
+    {
+      data: "fullname",
+      render: (data, type, row, meta) => {
+        if (type === "display") {
+          return `<input onchange="update_ctv('${row.username}', this.value, 1, '${data}', this)"  value="${data}" />`;
+        }
+        return data;
+      },
+    },
+    {
+      data: "image",
+      render: (data, type, row, meta) => {
+        const src = data || "/assets/img/default.jpg";
+        return ` <img
+              id="img_ncc_${row.username}"
+              src=${src}
+              onclick="setImgCtv('${row.username}')"
+              width="60"
+              height="60"
+              alt=""
+              style="cursor:pointer"
+              className="img img-thumbnail pull-left"
+            /><input id="file_ctv_${row.username}" type="file" style="display: none" onchange="update_ctv('${row.username}', this.files[0], 2, '${data}', this)" />`;
+      },
+    },
+    {
+      data: "money",
+      render: (data, type, row, meta) => {
+        return `<div style="width: 250px;">${data
+          .toString()
+          .replace(/\B(?=(\d{3})+(?!\d))/g, ",")} VNĐ</div>`;
+      },
+    },
+    {
+      data: "sex",
+      render: (data, type, row, meta) => {
+        if (type === "display") {
+          return `<select onchange="update_ctv('${
+            row.username
+          }', this.value, 3, '${data}', this)" style="width: 100px;">
+                      <option value="Nam" ${
+                        data === "Nam" && "selected"
+                      } >Nam</option>
+                      <option value="Nữ" ${
+                        data === "Nữ" && "selected"
+                      } >Nữ</option>
+                      <option value="Khác" ${
+                        data === "Khác" && "selected"
+                      } >Khác</option>
+                  </select>`;
+        }
+        return data;
+      },
+    },
+    {
+      data: "email",
+      render: (data, type, row, meta) => {
+        if (type === "display") {
+          return `<input onchange="update_ctv('${row.username}', this.value, 3, '${data}', this)" style="width: 300px;"  value="${data}" />`;
+        }
+        return data;
+      },
+    },
+    {
+      data: "sdt",
+      render: (data, type, row, meta) => {
+        if (type === "display") {
+          return `<input onchange="update_ctv('${row.username}', this.value, 4, '${data}', this)"  value="${data}" />`;
+        }
+        return data;
+      },
+    },
+    {
+      data: "address",
+      render: (data, type, row, meta) => {
+        if (type === "display") {
+          return `<input onchange="update_ctv('${row.username}', this.value, 6, '${data}', this)"  value="${data}" />`;
+        }
+        return data;
+      },
+    },
+    {
+      data: "createdate",
+      render: (data, type, row, meta) => {
+        if (type === "display") {
+          return `<div style="width: 150px;">${data}</div>`;
+        }
+        return data;
+      },
+    },
+  ],
+  columnDefs: [
+    // { targets: [0], width: "10px" },
+    // { targets: "_all", width: "130px" },
+    { className: "text-center", targets: [6, 7] },
+  ],
+};
+export default configCtv;
